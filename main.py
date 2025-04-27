9826c25d6cd8f5b988bd368e982acf9de53e6915
sinthushan <sinthushan@gmail.com>
2025-04-27 17:01:35 -0400

    added completer to the TO field

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import os
 import sys
-from PySide6.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QFileDialog, QComboBox
+from PySide6.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QFileDialog,  QCompleter, QLineEdit, QLabel
+from PySide6.QtCore import QStringListModel, Qt
+import re
+
 
 class Document:
     def __init__(self, path, has_headers):
@@ -9,6 +12,7 @@ class Document:
     def load(self, path: str, has_headers: bool):
         lines = []
         headers = []
+        body = []
         if os.path.exists(path):
             with open(path) as f:
                 lines = f.readlines()
@@ -16,8 +20,9 @@ class Document:
                 headers = list(range(0, len(lines[0].split(','))))
                 if has_headers:    
                     headers = lines[0].split(',')
-            return headers, lines    
-        return 0, lines
+                    body = lines[1:]
+            return headers, body    
+        return 0, body
 
 class DistributionList(Document):
     
@@ -31,24 +36,53 @@ class LoadDistributionListButton(QPushButton):
         self.clicked.connect(self.handleClick)
     def handleClick(self):
         file_path, _ = QFileDialog.getOpenFileName(self.parent, "Select the distribution list")
-        self.parent.distributionList = DistributionList(file_path, False)
-        self.parent.combo.addItems(map(str, self.parent.distributionList.headers))
+        self.parent.distributionList = DistributionList(file_path, True)
+        self.parent.model.setStringList(self.parent.distributionList.headers)
+        
+class FieldCompleter(QCompleter):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+    
+    def pathFromIndex(self, index):
+        path = super().pathFromIndex(index)
+        currentText = self.completionPrefix()
+        if currentText[-1].strip():
+            prefix = ' '.join(currentText.split(' ')[:-1])
+            return prefix + " {{" + path.strip() + "}}"
+        return currentText + "{{" + path.strip() + "}}" 
+    
+    def splitPath(self, path):
+        words = re.split(r'\s+', path)
+        if words:
+            return [words[-1]]
+        return [""]
 
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
-        
+
         self.documents = []
 
-        self.combo = QComboBox()
+        self.label = QLabel("To:")
+        
+
+        self.line_edit = QLineEdit()
+        
+        self.model = QStringListModel([])
+        self.completer = FieldCompleter()
+        self.completer.setModel(self.model)
+        self.completer.setFilterMode(Qt.MatchContains)
+
+        self.line_edit.setCompleter(self.completer)
+
         self.setWindowTitle("Email Burst")
         self.resize(300, 200)
         
         self.load_dist_list_btn = LoadDistributionListButton(self ,"Load Distribution List")
         self.layout = QVBoxLayout(self)
         self.layout.addWidget(self.load_dist_list_btn)
-        self.layout.addWidget(self.combo)
-
+        self.layout.addWidget(self.label)
+        self.layout.addWidget(self.line_edit)
 
 
 
